ee7065408ee197f1da5f0578e94533ab07ab5fa1
Coppelia <mfreese@gmx.ch>
2023-10-02 20:37:26 +0200

    comment

--- sandboxScript.py
+++ sandboxScript.py
@@ -2,7 +2,7 @@
 
 #luaExec require('sandboxScript')
 
-#Python runs just so that we have _pythonEvalExec
+#Python runs just so that we have _evalExec and similar
 
 def sysCall_init():
     sim = require('sim')
