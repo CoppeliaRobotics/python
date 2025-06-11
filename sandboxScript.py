c24391c2a2acf628a13dfd198fd1a8bfb376dd57
Coppelia <mfreese@gmx.ch>
2025-06-11 11:04:34 +0200

    sim-2

--- sandboxScript.py
+++ sandboxScript.py
@@ -5,4 +5,5 @@
 #Python runs just so that we have _evalExec and similar
 
 def sysCall_init():
-    sim = require('sim')
+    sim = require('sim-2')
+    sim.addLog(sim.verbosity_warnings, 'sim-2 has been loaded from Python')
