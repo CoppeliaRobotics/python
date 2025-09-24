9062771ae11f373fefe641548754c01efdc9d752
Coppelia <mfreese@gmx.ch>
2025-09-24 14:57:08 +0200

    leave if coppeliaSim is not alive

--- pythonLauncher.py
+++ pythonLauncher.py
@@ -1,6 +1,8 @@
 import sys
-
+import psutil
 import zmq
+import sys
+
 
 try:
     import cbor2 as cbor
@@ -10,27 +12,43 @@ except ModuleNotFoundError:
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind(sys.argv[1])
+socket.setsockopt(zmq.RCVTIMEO, 1)
+PID = int(sys.argv[2])
 module = {}
-while True:
-    # in: cmd + (code or func+args), out: ret or err
-    req = cbor.loads(socket.recv())
-    rep = {}
-    if req['cmd'] == 'loadCode':
-        try:
-            code = compile(req['code'], req['info'], "exec") #__EXCEPTION__
-            exec(code,module) #__EXCEPTION__
-            rep = {'ret': None}
-        except Exception as e:
-            import traceback
-            rep = {'err': traceback.format_exc()}
-    elif req['cmd'] == 'callFunc':
-        try:
-            func = module[req['func']]
-            rep = {'ret': func(*req['args'])} #__EXCEPTION__
-        except Exception as e:
-            import traceback
-            rep = {'err': traceback.format_exc()}
-    else:
-        rep = {'err': f'unknown command: "{req["cmd"]}"'}
 
-    socket.send(cbor.dumps(rep))
+def isCoppeliaSimAlive():
+    try:
+        proc = psutil.Process(PID)
+        if proc.is_running() and proc.status() != psutil.STATUS_ZOMBIE and proc.name() == 'coppeliaSim':
+            return True
+    except (psutil.NoSuchProcess, psutil.AccessDenied):
+        pass
+    return False    
+
+while True:
+    try:
+        # in: cmd + (code or func+args), out: ret or err
+        raw = socket.recv()
+        req = cbor.loads(raw)
+        rep = {}
+        if req['cmd'] == 'loadCode':
+            try:
+                code = compile(req['code'], req['info'], "exec") #__EXCEPTION__
+                exec(code,module) #__EXCEPTION__
+                rep = {'ret': None}
+            except Exception as e:
+                import traceback
+                rep = {'err': traceback.format_exc()}
+        elif req['cmd'] == 'callFunc':
+            try:
+                func = module[req['func']]
+                rep = {'ret': func(*req['args'])} #__EXCEPTION__
+            except Exception as e:
+                import traceback
+                rep = {'err': traceback.format_exc()}
+        else:
+            rep = {'err': f'unknown command: "{req["cmd"]}"'}
+        socket.send(cbor.dumps(rep))
+    except zmq.Again:
+        if isCoppeliaSimAlive():
+            continue
